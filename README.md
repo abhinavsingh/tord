9fb55339f15e86371ebebd3d2932719c5eb7d898
Abhinav Singh <mailsforabhinav@gmail.com>
2014-02-06 14:46:16 +0530

    bump async_pubsub version which had import bug while installing from requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Asynchronous websocket + pubsub based web framework
+Asynchronous websocket + pubsub based web framework using tornado, redis, zmq, sockjs
--- tord/__init__.py
+++ tord/__init__.py
@@ -2,7 +2,7 @@ from .tord import Application
 
 VERSION = (0, 2, 3)
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
-__description__ = 'Asynchronous websocket + pubsub based web framework'
+__description__ = 'Asynchronous websocket + pubsub based web framework using tornado, redis, zmq, sockjs'
 __author__ = 'Abhinav Singh'
 __author_email__ = 'mailsforabhinav@gmail.com'
 __homepage__ = 'https://github.com/abhinavsingh/tord'
