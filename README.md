68b67d2e582c9ac5ed5a7c78988d312c5d51c263
Abhinav Singh <mailsforabhinav@gmail.com>
2014-02-28 19:04:52 +0530

    added console.assert tests within main.js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Asynchronous websocket + pubsub based web framework using tornado, redis, zmq, sockjs
+
+Usage:
+------
+
+$ python example/example.py
+$ open http://localhost:8888/
+
+See javascript console and example/static/main.js for usage details.
--- example/static/main.js
+++ example/static/main.js
@@ -1,4 +1,24 @@
 $(function() {
 	window.tord = new Tord.Channel();
 	tord.connect();
+	
+	var id1 = tord.request('/api/user/1/', {'a':'ç'}, function(msg) {
+		console.assert(msg._id_ == id1);
+		console.assert(msg._data_.user_id == '1');
+		console.assert(msg._path_ == '/api/user/1/');
+	});
+	
+	var id2 = tord.request('/api/user/1/photo/', {'a':'≈'}, function(msg) {
+		console.assert(msg._async_ == true);
+		console.assert(msg._id_ == id2);
+		console.assert(msg._data_.user_id == '1');
+		console.assert(msg._path_ == '/api/user/1/photo/');
+	});
+	
+	var id3 = tord.request('/api/user/1/streaming/', {'a':'√'}, function(msg) {
+		console.assert(msg._async_ == true);
+		if(msg._data_.i < 5)
+			console.assert(msg._final_ == false);
+		console.assert(msg._data_.stream == 'streaming');
+	});
 });
